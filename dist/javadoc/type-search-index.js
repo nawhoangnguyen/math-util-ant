bbaeef58a317ecbba7e974c4b646ccdff5807fa6
Namtayngang <nawaniac133@gmail.com>
2023-10-02 15:10:24 +0700

    add screenshot folder

---
+++ dist/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.giaolang.mathutil.main","l":"Main"},{"p":"com.giaolang.mathutil.core","l":"MathUtil"}];updateSearchResults();
